b7b30e8970f8bbcb644cfc77fa2f164e53160a59
Miu-lruma <lkviice@gmail.com>
2025-03-16 02:41:53 -0500

    CORS PLEASE WORK WITH ME

--- LilysMovieServer/Program.cs
+++ LilysMovieServer/Program.cs
@@ -4,6 +4,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins",
+        builder =>
+        {
+            builder.WithOrigins("https://miu-lruma.github.io") // Replace with your allowed origins
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,6 +31,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Use CORS middleware
+app.UseCors("AllowSpecificOrigins");
 
 app.UseAuthorization();
 
